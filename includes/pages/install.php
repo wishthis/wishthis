44c1d26d8945325a627a24c831780cbbcaa37800
grandeljay <github.jay@grandel.anonaddy.me>
2021-11-14 09:12:49 +0100

    Improve installer

--- includes/pages/install.php
+++ includes/pages/install.php
@@ -11,66 +11,74 @@ use wishthis\{Page, Database};
 $page = new page(__FILE__, 'Home');
 $page->header();
 
-if (isset($_POST['action']) && 'install' === $_POST['action']) {
-    $configDirectory = 'includes/config';
-    $configPath = $configDirectory . '/config.php';
-    $configSamplePath = $configDirectory . '/config-sample.php';
-    $configContents = file_get_contents($configSamplePath);
+$step = isset($_POST['step']) ? $_POST['step'] : 1;
 
-    foreach ($_POST as $key => $value) {
-        if ('DATABASE' === substr($key, 0, 8)) {
-            $configContents = preg_replace('/(' . $key . '.+?\').*?(\')/', '$1' . $value . '$2', $configContents);
+switch ($step) {
+    case 1:
+        ?>
+        <main>
+            <section>
+                <h1>Install</h1>
+                <h2>Step <? $step ?></h2>
+                <p>Welcome to the wishthis installer.</p>
+                <p>wishthis needs a database to function properly. Please enter your credentials.</p>
+
+                <form method="post">
+                    <input type="hidden" name="action" value="install" />
+                    <input type="hidden" name="step" value="<?= $step + 1; ?>" />
+
+                    <fieldset>
+                        <label>Host</label>
+                        <input type="text" name="DATABASE_HOST" placeholder="localhost" value="localhost" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label>Name</label>
+                        <input type="text" name="DATABASE_NAME" placeholder="withthis" value="withthis" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label>Username</label>
+                        <input type="text" name="DATABASE_USER" placeholder="root" value="root" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label>Password</label>
+                        <input type="text" name="DATABASE_PASSWORD" />
+                    </fieldset>
+
+                    <input type="submit" value="Install" />
+                </form>
+            </section>
+        </main>
+        <?php
+        break;
+
+    case 2:
+        $configDirectory = 'includes/config';
+        $configPath = $configDirectory . '/config.php';
+        $configSamplePath = $configDirectory . '/config-sample.php';
+        $configContents = file_get_contents($configSamplePath);
+
+        foreach ($_POST as $key => $value) {
+            if ('DATABASE' === substr($key, 0, 8)) {
+                $configContents = preg_replace('/(' . $key . '.+?\').*?(\')/', '$1' . $value . '$2', $configContents);
+            }
         }
-    }
 
-    file_put_contents($configPath, $configContents);
+        file_put_contents($configPath, $configContents);
 
-    ?>
-    <main>
-        <section>
-            <h1>Success</h1>
-            <p>wishthis has been successfully installed.</p>
+        ?>
+        <main>
+            <section>
+                <h1>Success</h1>
+                <p>wishthis has been successfully installed.</p>
 
-            <a class="button primary" href="">Continue</a>
-        </section>
-    </main>
-    <?php
-} else {
-    ?>
-    <main>
-        <section>
-            <h1>Install</h1>
-            <p>Welcome to the wishthis installer.</p>
-            <p>wishthis needs a database to function properly. Please enter your credentials.</p>
-
-            <form method="post">
-                <input type="hidden" name="action" value="install" />
-
-                <fieldset>
-                    <label>Host</label>
-                    <input type="text" name="DATABASE_HOST" placeholder="localhost" value="localhost" />
-                </fieldset>
-
-                <fieldset>
-                    <label>Name</label>
-                    <input type="text" name="DATABASE_NAME" placeholder="withthis" value="withthis" />
-                </fieldset>
-
-                <fieldset>
-                    <label>Username</label>
-                    <input type="text" name="DATABASE_USER" placeholder="root" value="root" />
-                </fieldset>
-
-                <fieldset>
-                    <label>Password</label>
-                    <input type="text" name="DATABASE_PASSWORD" />
-                </fieldset>
-
-                <input type="submit" value="Install" />
-            </form>
-        </section>
-    </main>
-    <?php
+                <a class="button primary" href="">Continue</a>
+            </section>
+        </main>
+        <?php
+        break;
 }
 
 $page->footer();
