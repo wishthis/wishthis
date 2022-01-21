65344edc3538bb5323f8203834451f798f9ee44f
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2022-01-21 16:38:51 +0100

    Update create-release.ps1

--- create-release.ps1
+++ create-release.ps1
@@ -6,7 +6,7 @@ $VersionRegEx = "define\('VERSION', '([\d\.]+)'\);"
 
 $IndexPHP = (Get-Content -Path .\index.php -Raw) -Replace $VersionRegEx, "`define('VERSION', '$Version');" | Set-Content -Path .\index.php
 
-IEX 'composer install --no-dev'
-IEX 'npm install --only=production --no-optional'
+IEX 'composer update --no-dev --with-all-dependencies --ignore-platform-req=php'
+IEX 'npm update --only=production --no-optional'
 
 Read-Host
