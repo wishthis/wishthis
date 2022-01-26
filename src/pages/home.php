fd089e28ccfc756fb84ea788e46d32f4204cf700
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2022-01-26 13:24:57 +0100

    Add use case

--- src/pages/home.php
+++ src/pages/home.php
@@ -20,11 +20,21 @@ $page->navigation();
         <div class="ui segment">
             <h2 class="ui header">Welcome to wishthis</h2>
             <p>
-                wishthis is a simple, intuitive and modern plattform to create,
+                wishthis is a simple, intuitive and modern platform to create,
                 manage and view your wishes for any kind of occasion.
             </p>
             <p>
                 Go ahead and get started now and <a href="/?page=wishlist-create">create a wishlist</a>!
+            </p>
+
+            <h2 class="ui header">Use case</h2>
+            <p>
+                Your birthday is coming up and you just created a wishlist with
+                all the cool stuff you want. Your friends and family want to
+                make sure you get something you are happy with so you send them
+                your wishlist link and if anybody decides to get an item for
+                you, they simply commit to buying it and the item disappears for
+                everybody else.
             </p>
         </div>
     </div>
