e19d2c018efafdc62a98c852d636920972e78980
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2022-02-24 08:45:44 +0100

    Add favicon

--- src/classes/page.php
+++ src/classes/page.php
@@ -139,6 +139,16 @@ class Page
 
             <link rel="manifest" href="manifest.json" />
 
+            <link rel="apple-touch-icon" sizes="180x180" href="/src/assets/img/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/src/assets/img/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/src/assets/img/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/src/assets/img/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/src/assets/img/favicon/safari-pinned-tab.svg" color="#5829bb" />
+            <link rel="shortcut icon" href="/src/assets/img/favicon/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-config" content="/src/assets/img/favicon/browserconfig.xml" />
+            <meta name="theme-color" content="#f4f4f4" />
+
             <?php
             /**
              * Stylesheets
