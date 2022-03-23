dc247956af8dfbdfa7a9fccb3578b70152a5f7b6
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2022-03-23 10:33:16 +0100

    Increase password length requirements

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -1,9 +1,9 @@
 $(function() {
     $('.ui.form').form({
         fields: {
-            email: 'email',
-            password: ['minLength[6]', 'empty'],
-            planet: ['minLength[3]', 'empty'],
+            email    : 'email',
+            password : ['minLength[8]', 'empty'],
+            planet   : ['minLength[3]', 'empty'],
         }
     });
 });
