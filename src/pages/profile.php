6a17231b0bd9d8eb89b3f854d1d68bcc5ea01977
grandeljay <github.jay@grandel.anonaddy.me>
2022-04-05 20:01:56 +0200

    Add tabs

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -60,73 +60,309 @@ $page->navigation();
     <div class="ui container">
         <h1 class="ui header"><?= $page->title ?></h1>
 
-        <div class="ui segment">
-            <form class="ui form" method="POST">
-                <input type="hidden" name="user-id" value="<?= $user->id ?>" />
-
-                <div class="field">
-                    <label><?= __('Email') ?></label>
-
-                    <input type="email" name="user-email" value="<?= $user->email ?>" />
-                </div>
-
-                <div class="two fields">
-                    <div class="field">
-                        <label><?= __('Password') ?></label>
-
-                        <input type="password" name="user-password" autocomplete="new-password" />
+        <div class="ui stackable grid">
+            <div class="four wide column">
+                <div class="ui vertical pointing fluid menu profile">
+                    <div class="item" data-tab="personal">
+                        <h4 class="ui header">Personal</h4>
+                        <p>Information regarding yourself</p>
                     </div>
-
-                    <div class="field">
-                        <label><?= __('Password (repeat)') ?></label>
-
-                        <input type="password" name="user-password-repeat" autocomplete="new-password" />
+                    <div class="item" data-tab="password">
+                        <h4 class="ui header">Password</h4>
+                        <p>Change your password</p>
+                    </div>
+                    <div class="item" data-tab="language">
+                        <h4 class="ui header">Language</h4>
+                        <p>Your language preferences</p>
                     </div>
                 </div>
+            </div>
 
-                <div class="two fields">
-                    <div class="field">
-                        <label><?= __('Birthdate') ?></label>
+            <div class="twelve wide stretched column">
+                <div class="ui tab" data-tab="personal">
+                    <div class="ui segment">
 
-                        <div class="ui calendar">
-                            <div class="ui input left icon">
-                                <i class="calendar icon"></i>
-                                <input type="text"
-                                    name="user-birthdate"
-                                    placeholder="<?= __('Pick a date') ?>"
-                                    value="<?= $user->birthdate ?>"
-                                />
+                        <form class="ui form" method="POST">
+                            <input type="hidden" name="user-id" value="<?= $user->id ?>" />
+
+                            <div class="three fields">
+                                <div class="field">
+                                    <label><?= __('First name') ?></label>
+
+                                    <input type="text" name="user-name-first" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Last name') ?></label>
+
+                                    <input type="text" name="user-name-last" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Nickname') ?></label>
+
+                                    <input type="text" name="user-name-nick" value="" />
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div class="field">
-                        <label><?= __('Language') ?></label>
+                            <div class="field">
+                                <label><?= __('Email') ?></label>
 
-                        <select class="ui search dropdown" name="user-locale">
-                            <?php if (!in_array('en', $locales)) { ?>
-                                <option value="<?= 'en' ?>"><?= \Locale::getDisplayName('en', $user->locale) ?></option>
-                            <?php } ?>
+                                <input type="email" name="user-email" value="<?= $user->email ?>" />
+                            </div>
 
-                            <?php foreach ($locales as $locale) { ?>
-                                <?php if ($locale === $user->locale) { ?>
-                                    <option value="<?= $locale ?>" selected><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
-                                <?php } else { ?>
-                                    <option value="<?= $locale ?>"><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
-                                <?php } ?>
-                            <?php } ?>
-                        </select>
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Password') ?></label>
+
+                                    <input type="password" name="user-password" autocomplete="new-password" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Password (repeat)') ?></label>
+
+                                    <input type="password" name="user-password-repeat" autocomplete="new-password" />
+                                </div>
+                            </div>
+
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Birthdate') ?></label>
+
+                                    <div class="ui calendar">
+                                        <div class="ui input left icon">
+                                            <i class="calendar icon"></i>
+                                            <input type="text"
+                                                name="user-birthdate"
+                                                placeholder="<?= __('Pick a date') ?>"
+                                                value="<?= $user->birthdate ?>"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Language') ?></label>
+
+                                    <select class="ui search dropdown" name="user-locale">
+                                        <?php if (!in_array('en', $locales)) { ?>
+                                            <option value="<?= 'en' ?>"><?= \Locale::getDisplayName('en', $user->locale) ?></option>
+                                        <?php } ?>
+
+                                        <?php foreach ($locales as $locale) { ?>
+                                            <?php if ($locale === $user->locale) { ?>
+                                                <option value="<?= $locale ?>" selected><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } else { ?>
+                                                <option value="<?= $locale ?>"><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } ?>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="ui error message"></div>
+
+                            <input class="ui primary button"
+                                type="submit"
+                                value="<?= __('Save') ?>"
+                                title="<?= __('Save') ?>"
+                            />
+                        </form>
+
                     </div>
                 </div>
 
-                <div class="ui error message"></div>
+                <div class="ui tab" data-tab="password">
+                    <div class="ui segment">
 
-                <input class="ui primary button"
-                       type="submit"
-                       value="<?= __('Save') ?>"
-                       title="<?= __('Save') ?>"
-                />
-            </form>
+                        <form class="ui form" method="POST">
+                            <input type="hidden" name="user-id" value="<?= $user->id ?>" />
+
+                            <div class="three fields">
+                                <div class="field">
+                                    <label><?= __('First name') ?></label>
+
+                                    <input type="text" name="user-name-first" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Last name') ?></label>
+
+                                    <input type="text" name="user-name-last" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Nickname') ?></label>
+
+                                    <input type="text" name="user-name-nick" value="" />
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <label><?= __('Email') ?></label>
+
+                                <input type="email" name="user-email" value="<?= $user->email ?>" />
+                            </div>
+
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Password') ?></label>
+
+                                    <input type="password" name="user-password" autocomplete="new-password" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Password (repeat)') ?></label>
+
+                                    <input type="password" name="user-password-repeat" autocomplete="new-password" />
+                                </div>
+                            </div>
+
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Birthdate') ?></label>
+
+                                    <div class="ui calendar">
+                                        <div class="ui input left icon">
+                                            <i class="calendar icon"></i>
+                                            <input type="text"
+                                                name="user-birthdate"
+                                                placeholder="<?= __('Pick a date') ?>"
+                                                value="<?= $user->birthdate ?>"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Language') ?></label>
+
+                                    <select class="ui search dropdown" name="user-locale">
+                                        <?php if (!in_array('en', $locales)) { ?>
+                                            <option value="<?= 'en' ?>"><?= \Locale::getDisplayName('en', $user->locale) ?></option>
+                                        <?php } ?>
+
+                                        <?php foreach ($locales as $locale) { ?>
+                                            <?php if ($locale === $user->locale) { ?>
+                                                <option value="<?= $locale ?>" selected><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } else { ?>
+                                                <option value="<?= $locale ?>"><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } ?>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="ui error message"></div>
+
+                            <input class="ui primary button"
+                                type="submit"
+                                value="<?= __('Save') ?>"
+                                title="<?= __('Save') ?>"
+                            />
+                        </form>
+
+                    </div>
+                </div>
+
+                <div class="ui tab" data-tab="password">
+                    <div class="ui segment">
+
+                        <form class="ui form" method="POST">
+                            <input type="hidden" name="user-id" value="<?= $user->id ?>" />
+
+                            <div class="three fields">
+                                <div class="field">
+                                    <label><?= __('First name') ?></label>
+
+                                    <input type="text" name="user-name-first" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Last name') ?></label>
+
+                                    <input type="text" name="user-name-last" value="" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Nickname') ?></label>
+
+                                    <input type="text" name="user-name-nick" value="" />
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <label><?= __('Email') ?></label>
+
+                                <input type="email" name="user-email" value="<?= $user->email ?>" />
+                            </div>
+
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Password') ?></label>
+
+                                    <input type="password" name="user-password" autocomplete="new-password" />
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Password (repeat)') ?></label>
+
+                                    <input type="password" name="user-password-repeat" autocomplete="new-password" />
+                                </div>
+                            </div>
+
+                            <div class="two fields">
+                                <div class="field">
+                                    <label><?= __('Birthdate') ?></label>
+
+                                    <div class="ui calendar">
+                                        <div class="ui input left icon">
+                                            <i class="calendar icon"></i>
+                                            <input type="text"
+                                                name="user-birthdate"
+                                                placeholder="<?= __('Pick a date') ?>"
+                                                value="<?= $user->birthdate ?>"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label><?= __('Language') ?></label>
+
+                                    <select class="ui search dropdown" name="user-locale">
+                                        <?php if (!in_array('en', $locales)) { ?>
+                                            <option value="<?= 'en' ?>"><?= \Locale::getDisplayName('en', $user->locale) ?></option>
+                                        <?php } ?>
+
+                                        <?php foreach ($locales as $locale) { ?>
+                                            <?php if ($locale === $user->locale) { ?>
+                                                <option value="<?= $locale ?>" selected><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } else { ?>
+                                                <option value="<?= $locale ?>"><?= \Locale::getDisplayName($locale, $user->locale) ?></option>
+                                            <?php } ?>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="ui error message"></div>
+
+                            <input class="ui primary button"
+                                type="submit"
+                                value="<?= __('Save') ?>"
+                                title="<?= __('Save') ?>"
+                            />
+                        </form>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="ui segment">
+            asdf
         </div>
     </div>
 </main>
