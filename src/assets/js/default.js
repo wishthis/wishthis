3eeb808482fcac2ccbb98c03c563bd7eeb0ad80f
grandeljay <github.jay@grandel.anonaddy.me>
2022-07-07 13:53:41 +0200

    Fix PWA service worker

--- src/assets/js/default.js
+++ src/assets/js/default.js
@@ -4,9 +4,9 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
     navigator.serviceWorker
-        .register('/serviceWorker.js')
-        .then(res => console.log('service worker registered'))
-        .catch(err => console.log('service worker not registered', err))
+        .register('/src/assets/js/service-worker.js')
+        .then(res => console.log('wishthis service worker registered'))
+        .catch(err => console.log('wishthis service worker not registered', err))
     })
 }
 
