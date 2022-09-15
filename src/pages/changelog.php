fcf1c9a80d4a25315c7c5b2d66788905c96a3c44
grandeljay <github.jay@grandel.anonaddy.me>
2022-09-15 19:36:50 +0200

    Improve localisation

--- src/pages/changelog.php
+++ src/pages/changelog.php
@@ -38,36 +38,126 @@ $page->navigation();
                         <div class="ui segment">
                             <h3 class="ui header"><?= __('Added') ?></h3>
                             <ul>
-                                <li><?= __('Blog') ?></li>
-                                <li><?= __('Dark theme') ?></li>
-                                <li><?= __('Wish products') ?></li>
-                                <li><?= __('Jump to last edited wishlist from home') ?></li>
-                                <li><?= __('Quick add wish from home') ?></li>
-                                <li><?= __('Button to request more wishes from a users wishlist') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Blog')
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Dark theme')
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Wish products')
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Jump to last edited wishlist from home')
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Quick add wish from home')
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Button to request more wishes from a users wishlist')
+                                    ?>
+                                </li>
                             </ul>
 
                             <h3 class="ui header"><?= __('Improved') ?></h3>
                             <ul>
-                                <li><?= __('Localisation (many new translations added)') ?></li>
-                                <li><?= __('Additional logins are no longer required when switching between wishthis channels') ?></li>
-                                <li><?= __('Remembered wishlists design') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Improved */
+                                        echo __('Localisation (many new translations added)');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Improved */
+                                        echo __('Additional logins are no longer required when switching between wishthis channels');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Improved */
+                                        echo __('Remembered wishlists design');
+                                    ?>
+                                </li>
                             </ul>
 
                             <h3 class="ui header"><?= __('Changed') ?></h3>
                             <ul>
-                                <li><?= __('Changelog is now a page instead of a downloadable markdown file') ?></li>
-                                <li><?= __('Wishes can be edited from the wishlist now, without loading another page') ?></li>
-                                <li><?= __('"Saved wishlists" has been renamed to "Remember lists"') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Changed */
+                                        echo __('Changelog is now a page instead of a downloadable markdown file');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Changed */
+                                        echo __('Wishes can be edited from the wishlist now, without loading another page');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Changed */
+                                        echo __('"Saved wishlists" has been renamed to "Remember lists"');
+                                    ?>
+                                </li>
                             </ul>
 
                             <h3 class="ui header"><?= __('Fixed') ?></h3>
                             <ul>
-                                <li><?= __('Various minor things (typos, menu order, etc)') ?></li>
-                                <li><?= __('Wish information being updated with 404 content from URL') ?></li>
-                                <li><?= __('Wish image not showing') ?></li>
-                                <li><?= __('An error when saving a wish with a really long URL') ?></li>
-                                <li><?= __('Redirect errors on Nginx') ?></li>
-                                <li><?= __('An error when fetching title from an URL containing quotes') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('Various minor things (typos, menu order, etc)');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('Wish information being updated with 404 content from URL');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('Wish image not showing');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('An error when saving a wish with a really long URL');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('Redirect errors on Nginx');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Fixed */
+                                        echo __('An error when fetching title from an URL containing quotes');
+                                    ?>
+                                </li>
                             </ul>
                         </div>
 
@@ -84,19 +174,44 @@ $page->navigation();
                         <div class="ui segment">
                             <h3 class="ui header"><?= __('Added') ?></h3>
                             <ul>
-                                <li><?= __('This changelog') ?></li>
-                                <li><?= __('Wish properties') ?></li>
-                                <li><?= __('Button to mark wish as fulfilled') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('This changelog');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Wish properties');
+                                    ?>
+                                </li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Added */
+                                        echo __('Button to mark wish as fulfilled');
+                                    ?>
+                                </li>
                             </ul>
 
                             <h3 class="ui header"><?= __('Improved') ?></h3>
                             <ul>
-                                <li><?= __('Card design') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Improved */
+                                        echo __('Card design');
+                                    ?>
+                                </li>
                             </ul>
 
                             <h3 class="ui header"><?= __('Fixed') ?></h3>
                             <ul>
-                                <li><?= __('Various small bugs') ?></li>
+                                <li>
+                                    <?php
+                                        /** TRANSLATORS: Changelog: Improved */
+                                        echo __('Various small bugs');
+                                    ?>
+                                </li>
                             </ul>
                         </div>
 
