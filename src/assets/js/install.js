b49cbcd84fd8e4b8ac56fd6d7e722d0aa8c6078c
grandeljay <github.jay@grandel.anonaddy.me>
2022-10-12 19:45:26 +0200

    Improve installer

---
+++ src/assets/js/install.js
@@ -0,0 +1,36 @@
+$(function() {
+
+    /**
+     * Step 1
+     */
+    $(document).on('click', '#database-test', function(event) {
+        var form         = $(this).closest('form');
+        var formDatabase = new URLSearchParams(new FormData(form[0]));
+
+        form.addClass('loading');
+
+        fetch('/src/api/database-test.php', {
+            method : 'POST',
+            body   : formDatabase
+        })
+        .then(handleFetchError)
+        .then(handleFetchResponse)
+        .then(function(response) {
+            if (response.success) {
+                $('input[type="submit"]').removeClass('disabled');
+
+                form.form('remove errors');
+
+                $('body').toast({ message : 'Database connection succeeded.' });
+            } else {
+                $('input[type="submit"]').addClass('disabled');
+
+                form.form('add errors', ['Database connection failed.']);
+            }
+        })
+        .finally(function() {
+            form.removeClass('loading');
+        });
+    });
+
+});
