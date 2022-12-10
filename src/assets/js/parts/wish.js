121350ec613909510c242d0d73b024dd14638d01
grandeljay <github.jay@grandel.anonaddy.me>
2022-12-10 17:39:51 +0100

    Fix console output

--- src/assets/js/parts/wish.js
+++ src/assets/js/parts/wish.js
@@ -1,7 +1,6 @@
 $(function () {
 
     $(document).on('click', '.ui.button.details', function() {
-        console.log($('.ui.modal.details'));
         $('.ui.modal.details').modal('show');
     });
 
