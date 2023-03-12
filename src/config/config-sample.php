642b3f4a98ce50a96541959f8bae00338f84344e
grandeljay <github.jay@grandel.anonaddy.me>
2023-03-12 11:30:45 +0100

    docs(configuration): #64 add description to constants

--- src/config/config-sample.php
+++ src/config/config-sample.php
@@ -1,18 +1,34 @@
 <?php
 
 /**
- * config-sample.php
+ * Configuration
  *
- * @author Jay Trees <github.jay@grandel.anonaddy.me>
+ * Usually, the wishthis installer will create the config.php for you.
  */
 
+/**
+ * Database
+ *
+ * Used to connect to your MySQL database.
+ */
 define('DATABASE_HOST', 'localhost');
 define('DATABASE_NAME', 'wishthis');
 define('DATABASE_USER', 'root');
 define('DATABASE_PASSWORD', '');
 
+/**
+ * Development
+ *
+ * Keep this disabled for production sites.
+ */
 define('ENV_IS_DEV', false);
 
+/**
+ * Channels
+ *
+ * It's save to delete this if you are self-hosting. Alternatively you can
+ * replace these branches and domains with your own.
+ */
 define(
     'CHANNELS',
     array(
