06cf30bfb7047f136b23f869f3994c61d9390564
grandeljay <github@grandels.email>
2023-11-18 12:56:28 +0100

    chore: update to v1.1.1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 namespace wishthis;
 
-define('VERSION', '1.1.0');
+define('VERSION', '1.1.1');
 define('ROOT', __DIR__);
 define('DEFAULT_LOCALE', 'en_GB');
 define('COOKIE_PERSISTENT', 'wishthis_persistent');
