0c83f5ed92a11bc5ecd36767b88eedde8f1a019f
Jay Trees <github@grandels.email>
2025-07-08 14:58:12 +0200

    fix: remove `npm` from markup

--- src/pages/wishlists.php
+++ src/pages/wishlists.php
@@ -83,7 +83,7 @@ $page->navigation();
                         <?= __('Create a wishlist') ?>
                     </a>
                 </div>
-            </div>npm
+            </div>
         </div>
 
         <h2 class="ui header"><?= __('Wishes') ?></h2>
